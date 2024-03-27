2c080d3cd1eb007f182e5e349832da1e1717e1b2
Sheikh Atiqa <162320973+Sheikhatiqaofficial@users.noreply.github.com>
2024-03-28 02:14:57 +0500

    Add files via upload

---
+++ CLASS-6(logic statements )/logical.js
@@ -0,0 +1,24 @@
+var iscooking = true;
+var isordering = false;
+// IF:
+if (iscooking) {
+    console.log("eat a meal");
+}
+// ELSE IF:
+else if (isordering) {
+    console.log("dont eat");
+}
+// ELSE:
+else {
+    console.log("eat a junk meal");
+}
+var weather = "cloudy";
+if (weather === "raining") {
+    console.log("wear a rain coat");
+}
+else if (weather === "cold") {
+    console.log("wear a furr coat");
+}
+else {
+    console.log("wear sunglasses");
+}
---
+++ CLASS-6(logic statements )/logical.ts
@@ -0,0 +1,16 @@
+let iscooking = true;
+// IF:
+if (iscooking) { console.log(`eat a meal`);}
+// ELSE:
+else {console.log(`eat a junk meal`);}
+
+let weather = "cloudy";
+// IF:
+if (weather ==="raining") 
+{console.log("wear a rain coat");}
+// ELSE IF:
+else if (weather === "cold")
+{console.log("wear a furr coat");}
+// ELSE:
+else {console.log("wear sunglasses");}
+
